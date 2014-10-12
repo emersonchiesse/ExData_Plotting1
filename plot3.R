7e7c6270129db46ebc77756737faab4a839cfbb3
Emerson Chiesse <emersonchiesse@gmail.com>
2014-10-12 09:40:54 -0300

    correction on plot3

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-#download.file(url, destfile="power.zip", method="wget")
-#unzip("power.zip")
-#data<-read.table("household_power_consumption.txt", sep=";",header=T, colClasses=c("character", "character", "numeric", "numeric", "numeric"), na.strings="?")
+download.file(url, destfile="power.zip", method="wget")
+unzip("power.zip")
+data<-read.table("household_power_consumption.txt", sep=";",header=T, colClasses=c("character", "character", "numeric", "numeric", "numeric"), na.strings="?")
 
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
